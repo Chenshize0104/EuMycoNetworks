452e0bd57bd3c295cadfa7a3c5a3acc1a8f0806b
Li CHEN <73270372+Chenshize0104@users.noreply.github.com>
2025-01-03 09:42:46 +0800

    Update 6.Community_simulation.R

--- 6.Community_simulation.R
+++ 6.Community_simulation.R
@@ -14,14 +14,14 @@ head(design)
 # design <- subset(design,Plant_Species!="Euphorbia_jolkinii")
 design <- design[,1:8]
 
-network <- read.read_excel("data/network_matrix.xlsx", sheet="Sheet1")
+network <- read_excel("data/network_matrix.xlsx", sheet="Sheet1")
 head(network)
 # network <- subset(network,Plant_Species!="Euphorbia_jolkinii")
 
-network$lower <- network$AMFungi_OTUID
-network$higher <- network$Plant_Species
-network$webID_o <- paste0(network$Site,network$Plot)
-network$freq <- network$Frequency
+# network$lower <- network$AMFungi_OTUID
+# network$higher <- network$Plant_Species
+# network$webID_o <- paste0(network$Site,network$Plot)
+# network$freq <- network$Frequency
 
 network_matrix <- network
 network_matrix <- as.data.frame(network_matrix)
@@ -75,7 +75,6 @@ for (site in unique(design$Site)) {
 ########
 # 计算每个网络的1000个simulation的mean、sd
 simulations_result <- function(simulation_network, index){
-  # simul.value <- apply(simulation_network, 3, function(x)networklevel(x, index=index))
   simul.value <- unlist(sapply(simulation_network, networklevel, index=index))
   mean.simul <- mean(simul.value)
   sd.simul <- sd(simul.value)
@@ -132,134 +131,34 @@ save.image("results/network_analyses_simulation.RData")
 # save.image("results/network_analyses_simulation_Eup.jo_excluded.RData")
 
 #################################################################################
-# weighted modularity; 每个网络产生100个随机群落, 每个网络计算30次，取最大值
-design <- read_excel("data/design.xlsx", sheet="Sheet1")
-head(design)
-# design <- subset(design,Plant_Species!="Euphorbia_jolkinii")
-design <- design[,1:8]
-
-network <- read.read_excel("data/network_matrix.xlsx", sheet="Sheet1")
-head(network)
-# network <- subset(network,Plant_Species!="Euphorbia_jolkinii")
-
-network$lower <- network$AMFungi_OTUID
-network$higher <- network$Plant_Species
-network$webID <- paste0(network$Site,network$Plot)
-network$freq <- network$Frequency
-
-network_matrix <- network
-network_matrix <- as.data.frame(network_matrix)
-
-
-########
-# 对样地内所有样品（包括没有真菌的样品）进行随机化抽样
-
-# 耗时很长，10个样地分开算
-unique(design$Site)
-
-result_net1 <- list()
-result_net2 <- list()
-result_all <- list()
-for (site in unique(design$Site)){
-	design_Site <- subset(design,Site==site)
-	# dim(design_Site)[1]/2
-	# nrow(design_Site)/2
-
-	network_Site <- subset(network_matrix,Site==site)
-	# dim(network_Site)[1]/2
-	# nrow(network_Site)/2
-
-	set.seed(123)
-	sample_data <- design_Site
-	link_data <- network_Site
-
-	# 设置抽样次数
-	num_samples <- 100
-
-	# 创建一个空列表，用于存放抽样结果
-	sample_results <- vector("list", num_samples)
-
-	# 对数据库进行抽样100次
-	for (i in 1:num_samples) {
-	  # 随机抽样一半的sample
-	  sample_indices <- sample(1:nrow(sample_data), size = nrow(sample_data)/2, replace = FALSE)
-	  s1.sample <- sample_data[sample_indices, ]$Sample
-	  print(length(s1.sample))
-	  s1 <- link_data[link_data$Sample %in% s1.sample, ]
-	  length(unique(s1$Sample))
-	  s1$webID_simulation <- paste0(s1$Site,"_s1")
-	  # 将剩余的sample作为 s2
-	  s2.sample  <- sample_data[-sample_indices, ]$Sample
-	  length(s2.sample)
-	  s2 <- link_data[link_data$Sample %in% s2.sample, ]
-	  print(length(unique(s2$Sample)))
-	  s2$webID_simulation <- paste0(s2$Site,"_s2")
-	  simu_new <- rbind(s1,s2)
-	  # 将抽样结果保存到列表中
-	  sample_results[[i]] <- simu_new
-	}
-	# 检查抽样结果的前几个
-	head(sample_results[[1]])
-	network_result <- data.frame()
-	for (i in 1:num_samples) {
-	  link_comun <- sample_results[[i]]
-	  link_comun$webID_o <- link_comun$webID
-	  link_comun$webID <- link_comun$webID_simulation
-	  
-	  network <- frame2webs(link_comun,type.out= "list")
-	  
-	  #定量模块观察值，每个网络重复三十次，取最大值
-	  repeatmodule <- function(network,n){
-		likeliresult <- list()
-		m <- length(network)
-		for (i in 1:n) {
-		  likeliresult[[i]] <- sapply(network,function(x)computeModules(x, method="Beckett")@likelihood)
-		}
-		result <- t(matrix(unlist(likeliresult),m,n))
-		rownames(result) <- 1:n
-		colnames(result) <- names(network)
-		result
-	  }
-	  Wmodul <- repeatmodule(network,30)
-	  Wmodulmax <- t(t(apply(Wmodul,2,max)))
-	  colnames(Wmodulmax) <- "weighted modularity"
-	  network_result2 <- Wmodulmax
-	  network_result_all <- rbind(network_result,network_result2)
-	  network_result <- network_result_all
-	}
-
-	network_result_s1 <- network_result_all[grepl("_s1", rownames(network_result_all)), ]
-	network_result_s2 <- network_result_all[grepl("_s2", rownames(network_result_all)), ]
-
-	library(psych)
-	# ?describe
-	result1 <- describe(network_result_s1)
-	result2 <- describe(network_result_s2)
-	result_net1 <- list()
-	result_net2 <- list()
-	result_net1[[site]] <- result1
-	result_net2[[site]] <- result2
-
-	network_result_cha <- network_result_s1-network_result_s2
-	result <- describe(network_result_cha)
-
-	result_all <- list()
-	result_all[[site]] <- result
+# weighted modularity，计算每个网络的1000个simulation的mean、sd
+simulations_mod_result <- function(simulation_network){
+  simul.value <- unlist(sapply(simulation_network,function(x)computeModules(x, method="Beckett")@likelihood))
+  mean.simul <- mean(simul.value)
+  sd.simul <- sd(simul.value)
+  return(cbind(mean.simul, sd.simul))
 }
 
-write.csv(result_all,paste0("results/network_simulation_module", site, ".csv"))
-# write.csv(result_all,paste0("results/network_simulation_module", site, "_Eup.jo_excluded.csv"))
+Wmod_results <- list()
+for (i in names(simulation_network_all)) {
+  Wmod_results[[i]] <- simulations_mod_result(simulation_network_all[[i]])
+}
+Wmod_results
+Wmod_results.all <- do.call(rbind,Wmod_results)
+Wmod_results.all <- as.data.frame(Wmod_results.all)
+Wmod_results.all <- cbind(webID=names(simulation_network_all), Wmod_results.all)
+Wmod_results.all$simulation_index <- "weighted modularity"
+Wmod_results.all
 
-result_all.all1 <- do.call(rbind,result_net1)
-result_all.all2 <- do.call(rbind,result_net2)
 
-result_all.all.all <- cbind(result_all.all1,result_all.all2)
+simulation_data2 <- bind_rows(evenness_results.all,Wnodf_results.all,Wcon_results.all,Wmod_results.all)
+simulation_data2
 
-write.csv(result_all.all.all,paste0("results/network_simulation_module", site, "all.csv"))
-# write.csv(result_all.all.all,paste0("results/network_simulation_module", site, "all_Eup.jo_excluded.csv"))
+write.csv(simulation_data2,"results/network_analyses_simulation2.csv")
+# write.csv(simulation_data,"results/network_analyses_simulation_Eup.jo_excluded2.csv")
 
-save.image(paste0("results/network_simulation_module_", site, ".RData"))
-# save.image(paste0("results/network_simulation_module_", site, "_Eup.jo_excluded.RData"))
+save.image("results/network_analyses_simulation2.RData")
+# save.image("results/network_analyses_simulation_Eup.jo_excluded2.RData")
 
 #################################################################################
 #################################################################################
